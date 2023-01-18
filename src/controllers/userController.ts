fe0aaf1eb67bc43e5a60d9a5f1b8c0bf03f1b667
1canas <pedro.s.furlaneto@gmail.com>
2023-01-17 23:27:42 -0300

    feat(route): get user route implemented

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -5,96 +5,120 @@ import { User } from "../models/User";
 import bcrypt from "bcrypt";
 
 async function register(req: Request, res: Response) {
-    const dateObject = new Date();
+  const dateObject = new Date();
 
-    const { name, email, password, confirm_password } = req.body;
+  const { name, email, password, confirmPassword } = req.body;
 
-    if (!name) {
-        return res.status(422).json({
-            statusCode: 422,
-            message: "Expected 'name' field",
-            timestamp: dateObject.getTime()
-        });
-    }
+  if (!name) {
+    return res.status(422).json({
+      statusCode: 422,
+      message: "Expected 'name' field",
+      timestamp: dateObject.getTime(),
+    });
+  }
 
-    if (!email) {
-        return res.status(422).json({
-            statusCode: 422,
-            message: "Expected 'email' field",
-            timestamp: dateObject.getTime()
-        });
-    }
+  if (!email) {
+    return res.status(422).json({
+      statusCode: 422,
+      message: "Expected 'email' field",
+      timestamp: dateObject.getTime(),
+    });
+  }
 
-    if (!password) {
-        return res.status(422).json({
-            statusCode: 422,
-            message: "Expected 'passwword' field",
-            timestamp: dateObject.getTime()
-        });
-    }
+  if (!password) {
+    return res.status(422).json({
+      statusCode: 422,
+      message: "Expected 'passwword' field",
+      timestamp: dateObject.getTime(),
+    });
+  }
 
-    if (password !== confirm_password) {
-        return res.status(422).json({
-            statusCode: 422,
-            message: "Password and confirm password must be equals",
-            timestamp: dateObject.getDate()
-        });
-    }
+  if (password !== confirmPassword) {
+    return res.status(422).json({
+      statusCode: 422,
+      message: "Password and confirm password must be equals",
+      timestamp: dateObject.getDate(),
+    });
+  }
 
-    const matchedUser = await User.findOne({ email });
+  const matchedUser = await User.findOne({ email });
 
-    if (matchedUser) {
-        return res.status(422).json({
-            statusCode: 422,
-            message: "User already exists",
-            timestamp: dateObject.getTime()
-        });
-    }
+  if (matchedUser) {
+    return res.status(422).json({
+      statusCode: 422,
+      message: "User already exists",
+      timestamp: dateObject.getTime(),
+    });
+  }
 
-    const salt = await bcrypt.genSalt(11);
-    const saltedPassword = await bcrypt.hash(password, salt);
+  const salt = await bcrypt.genSalt(11);
+  const saltedPassword = await bcrypt.hash(password, salt);
 
-    try {
-        const user = new User({
-            email,
-            name,
-            password: saltedPassword
-        });
-        
-        const { _id } = await user.save({ timestamps: true });
+  try {
+    const user = new User({
+      email,
+      name,
+      password: saltedPassword,
+    });
 
-        return res.status(201).json({
-            statusCode: 201,
-            message: "User registered successfully",
-            id: _id,
-            timestamp: dateObject.getTime()
-        });
+    const { _id } = await user.save();
 
-    } catch (error) {
-        console.log(error);
+    return res.status(201).json({
+      statusCode: 201,
+      message: "User registered successfully",
+      id: _id,
+      timestamp: dateObject.getTime(),
+    });
+  } catch (error) {
+    console.log(error);
 
-        return res.status(500).json({
-            message: "Server internal error",
-            timestamp: dateObject.getTime()
-        });
-    }
+    return res.status(500).json({
+      message: "Server internal error",
+      timestamp: dateObject.getTime(),
+    });
+  }
 }
 
 async function get(req: Request, res: Response) {
-    
+  const dateObject = new Date();
+  const { id } = req.params;
+
+  if (!id) {
+    return res.status(422).json({
+      statusCode: 422,
+      message: "Expected 'id' param",
+      timestamp: dateObject.getTime(),
+    });
+  }
+
+  const findedUser = await User.findById(id, '-password');
+
+  if (!findedUser) {
+    return res.status(204).json({
+      statusCode: 204,
+      message: "User not found",
+      timestamp: dateObject.getTime(),
+    });
+  }
+
+  return res.status(200).json({
+    statusCode: 200,
+    user: findedUser,
+    timestamp: dateObject.getTime()
+  });
 }
 
 function remove(req: Request, res: Response) {
-    return res.status(401).send('não impl');
+  return res.status(401).send("não impl");
 }
 
 function update(req: Request, res: Response) {
-    return res.status(401).send('não impl');
+  return res.status(401).send("não impl");
 }
 
 export const userController = {
-    register,
-    get,
-    remove,
-    update
-}
+  register,
+  get,
+  remove,
+  update,
+};
